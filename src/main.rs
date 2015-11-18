32faec18d2b748ac199c65ee980d4f92c81f2c1d
Etienne Levesque Guitard <etiennelg@gmail.com>
2015-11-18 23:08:00 +0000

    More experiments

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,38 @@
 fn main() {
-
     let hachiko = "忠犬ハチ公".to_string();
-    slice(hachiko, 0, 1);
+    println!("{}", hachiko.slice(0, 1));
 }
 
+trait CanSlice {
+    fn slice(&self, range_start: i32, range_end: i32) -> String;
+}
+
+impl CanSlice for String {
+    fn slice(&self, range_start: i32, range_end: i32) -> String {
+        let mut range_counter = 0;
+        let mut string_buffer: String = "".to_string();
+
+        for c in self.to_string().chars() {
+
+            if range_counter < range_start {
+                range_counter += 1;
+                continue;
+            }
+
+            if range_counter > range_end {
+                break;
+            }
+
+            string_buffer.push_str(&*c.to_string());
+
+            range_counter += 1;
+        }
+
+        return string_buffer
+    }
+}
+
+/*
 fn slice(string: String, range_start: i32, range_end: i32) {
     
     let mut range_counter = 0;
@@ -24,3 +53,4 @@ fn slice(string: String, range_start: i32, range_end: i32) {
         range_counter += 1;
     }
 }
+*/
